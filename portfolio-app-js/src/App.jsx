fa67b629aba284f87135a60f9dcd1cf516f87dcf
vivekbigelow <vivekbigelow1234@gmail.com>
2022-09-28 23:47:15 -0400

    navbar to App

--- portfolio-app-js/src/App.jsx
+++ portfolio-app-js/src/App.jsx
@@ -1,8 +1,25 @@
 
+import Piece from './Piece';
+
+const apiUrl = "http://localhost:3001/pieces";
 
 function App() {
 
-  return <div>Portfolio-App-JS</div>
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            Home
+          </li>
+          <li>
+            Admin
+          </li>
+        </ul>
+      </nav>
+      <Piece title={"Smiley"} year={2022} medium={"digital collage"} description={"A design"} />
+    </>
+  )
 }
 
 export default App
